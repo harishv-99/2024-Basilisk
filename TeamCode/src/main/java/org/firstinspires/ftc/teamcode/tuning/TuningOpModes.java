cbc9e302f278836b12e252aa683472d354eb6076
lucasvashmunoz <vashmunozlucas@gmail.com>
2024-09-22 17:03:43 -0700

    roadrunner tuning - set motor names - set logo/usb direction of imu - specified ThreeDeadWheelLocalizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TuningOpModes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/TuningOpModes.java
@@ -33,6 +33,7 @@ import java.util.List;
 
 public final class TuningOpModes {
     // TODO: change this to TankDrive.class if you're using tank
+    // BASILISK: confirmed DRIVE_CLASS is MecanumDrive
     public static final Class<?> DRIVE_CLASS = MecanumDrive.class;
 
     public static final String GROUP = "quickstart";
