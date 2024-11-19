779650b668b5c213c6caaf5830d611395274ab21
virenFTC <virenpudi@gmail.com>
2024-11-18 16:48:58 -0800

    We started to test roadrunner actions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/TestArmHRV.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.robots;
+package org.firstinspires.ftc.teamcode.teleops;
+import androidx.annotation.NonNull;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import edu.ftcphoenix.fw.gamepad.GamepadController;
+import edu.ftcphoenix.fw.gamepad.GamepadInputs;
+import edu.ftcphoenix.fw.gamepad.GamepadKeys;
+import edu.ftcphoenix.fw.robotbase.periodicrunner.PeriodicRunner;
+
+@Autonomous
+public class TestArmHRV extends LinearOpMode {
+
+
+    public void runOpMode() {
+        if(slidemotorpos)
+    }
