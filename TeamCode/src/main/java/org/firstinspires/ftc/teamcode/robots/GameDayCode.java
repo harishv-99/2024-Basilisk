8f172a85759e5f5c8921765082b28fb7763e1d30
Harish Varanasi <hvaranas@fb.com>
2025-01-13 18:04:32 -0800

    Joystick calibration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/GameDayCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/GameDayCode.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 
-@TeleOp(name = "gamedaycodeoverridethingyclawversion2 (Blocks to Java)")
+@TeleOp(name = "GameDayCode")
 public class GameDayCode extends LinearOpMode {
 
     private DcMotor slideMotor;
